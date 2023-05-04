1323c47592c9f25d8041f1d62d81fa10eb2f062c
Siddharth Paneri <dpaneri3@gmail.com>
2023-05-04 17:28:44 +0530

    yup

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -28,8 +28,8 @@ const SignUpForm = () => {
         }
         try {
             const {user}=await createUserAuthWithEmailAndPassword(email, password)
-            // await createUserDocumentFromAuth(user,{displayName})
-            console.log(user)
+            await createUserDocumentFromAuth(user,{displayName})
+            
             resetFormFields()
         } catch (error) {
             if (error.code === "auth/email-already-in-use") alert("Email is already in use ")
